55f750e7f26615bb0debfb70fe56a29dd4ef71be
Leonardo <leonardosilvasantosss22@gmail.com>
2024-02-14 13:30:38 -0300

    Users POST request

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+
+const routes = require("./routes");
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
 
+app.use(routes);
